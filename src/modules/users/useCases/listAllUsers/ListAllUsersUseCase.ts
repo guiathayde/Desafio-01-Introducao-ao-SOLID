8016cae1132b1dbbaec53b9961f75dcca9a6df1b
Guilherme Athayde <guiathayde303@gmail.com>
2021-03-20 14:46:41 -0300

    Challenge finalized

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,13 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userIsAdmin = this.usersRepository.findById(user_id);
+
+    if (!userIsAdmin.admin && userIsAdmin) {
+      throw new Error("User is not admin!");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
